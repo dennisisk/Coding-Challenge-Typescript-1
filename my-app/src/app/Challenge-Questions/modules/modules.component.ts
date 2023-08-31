cc248d8c86974f87faf3f4ff9b53900094e0df6b
dennisisk <97694680+dennisisk@users.noreply.github.com>
2023-08-31 01:58:41 -0400

    Update modules.component.ts

--- my-app/src/app/Challenge-Questions/modules/modules.component.ts
+++ my-app/src/app/Challenge-Questions/modules/modules.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./modules.component.css']
 })
 export class ModulesComponent {
+  x: number = 5;
+  y: number = 5;
 
+  isLoaded: boolean = false;
+
+  firstName: string = 'John';
+  lastName: string = 'Doe';
+
+  pets: string[] = ['Fido', 'Lassie', 'Rover'];
+
+  info: any = { address: '1234 Anywhere St.', city: 'Seattle' };
+
+  testFunction() {
+    const z = this.x + this.y;
+    console.log('x + y = ' + z);
+    console.log('isLoaded = ' + this.isLoaded);
+    console.log('Pets List:');
+    for (const element of this.pets) {
+      console.log(element);
+    }
+    console.log('Info: ' + this.info.address + ' ' + this.info.city);
+  }
 }
